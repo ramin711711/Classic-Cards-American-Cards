aa47e92d8a7667488458f3955ba4a3bacf503e5d
ramin711711 <raminhuseynov@hotmail.com>
2024-09-04 19:28:03 +0400

    Add files via upload
    This is basically 2 different deck of cards combined. You get to choose which one you want to use, or if you want you can divide the code into 2 different files.

---
+++ Homework03.09.2024.py
@@ -0,0 +1,78 @@
+#1 A Classic Deck Of Cards:
+import random
+# a = int(input('Do you want to work with Classic deck of cards or the one with Jokers? (Type 1 for the Classic one | Type 2 for the one with Jokers.) '))
+# if a == 1:
+    # class Classic:
+    #     def __init__(self, face, suit):
+    #         self.face = face
+    #         self.suit = suit
+
+    #     def __repr__(self): #you can define your own string representation of your class objects
+    #         return "Face: " + self.face + " | Suit: " + self.suit
+    # all_cards = []
+    # for suit in ["clubs", "diamonds", "hearts", "spades"]:
+    #     for face in ["6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]:
+    #         all_cards.append(Classic(face, suit))  
+            
+    # card = Classic
+    # random.shuffle(all_cards)
+    # # trump = all_cards[-1][1]
+    # # print('The trump suit is:', trump)
+    # last_card = all_cards[-1]
+    # trump_face = last_card.face  
+    # trump_suit = last_card.suit
+    # print(f'Trump card is: {trump_face} of {trump_suit}\n')
+
+    # print("After shuffling:")
+    # for card in all_cards:
+    #     print(card)
+
+    # def all_of_the_cards(all_cards):
+    #     print("All of the cards: \n")
+    #     for card in all_cards:
+    #         print(card)
+
+    # st_card = all_cards[0]
+    # print("\nFirst card from the top is:", st_card)
+
+
+
+# elif input == 2:
+
+    #2 A Deck Of Cards With Joker: 
+    # import random
+    # class American:
+    #     def __init__(self, face, suit):
+    #         self.face = face
+    #         self.suit = suit
+
+    #     def __repr__(self): #you can define your own string representation of your class objects
+    #         return "Face: " + self.face + " | Suit: " + self.suit
+    # all_cards = []
+    # for suit in ["clubs", "diamonds", "hearts", "spades"]:
+    #     for face in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]:
+    #         all_cards.append(American(face, suit))  
+    # for color in ["Black", "Red"]:
+    #         all_cards.append(American(color, "Joker"))
+
+    # card = American
+    # random.shuffle(all_cards)
+    # last_card = all_cards[-1]
+    # trump_face = last_card.face  
+    # trump_suit = last_card.suit
+    # print(f'Trump card is: {trump_face} of {trump_suit}\n')
+    # print("After shuffling:")
+    # for card in all_cards:
+    #     print(card)
+
+    # def all_of_the_cards():
+    #     print("All of the cards: \n")
+    #     for card in all_cards:
+    #         print(card)
+        
+    # all_of_the_cards()
+
+    # st_card = all_cards[0]
+    # print("\nFirst card from the top is:", st_card)
+# else:
+#     print("Try running again and typing: 1 for Classic or 2 for the one with Jokers.")
